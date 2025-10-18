07e2cf8df556c47d97fe82b5a3fd904dfde64dda
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2025-10-18 15:19:41 +0300

    create page-container

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { resolve } from "path";
 import { glob } from "glob";
 import nunjucks from "vite-plugin-nunjucks";
@@ -6,7 +6,6 @@ import FullReload from "vite-plugin-full-reload";
 
 const pagesDir = resolve(__dirname, "src/pages");
 const outDir = "../../dist";
-const baseUrl = "/nuts/";
 
 function getHtmlEntries() {
   const entries = {};
@@ -20,39 +19,32 @@ function getHtmlEntries() {
   return entries;
 }
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
-    },
-  },
-  base: baseUrl,
-  root: pagesDir,
-  publicDir: resolve(__dirname, "src/shared/assets"),
-  build: {
-    outDir: outDir,
-    emptyOutDir: true,
-    rollupOptions: {
-      input: getHtmlEntries(),
-    },
-    assetsDir: "assets",
-  },
-  plugins: [
-    nunjucks({
-      variables: {
-        assetsPath: "/src",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const baseUrl = env.VITE_BASE_URL || "/";
+  console.log("baseUrl", baseUrl);
+  console.log("env", env);
+
+  return {
+    base: baseUrl,
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"),
       },
-    }),
-    FullReload(["src/**/*"]),
-  ],
-  // css: {
-  //   preprocessorOptions: {
-  //     scss: {
-  //       additionalData: `
-  //         @use "@/app/styles/variables" as *;
-  //         @use "@/app/styles/mixins" as *;
-  //       `,
-  //     },
-  //   },
-  // },
+    },
+    root: pagesDir,
+    publicDir: resolve(__dirname, "src/shared/assets"),
+    build: {
+      outDir: outDir,
+      emptyOutDir: true,
+      rollupOptions: {
+        input: getHtmlEntries(),
+      },
+      assetsDir: "assets",
+    },
+    plugins: [
+      nunjucks({ variables: { "*": { baseUrl: "yyyy" } } }),
+      FullReload(["src/**/*"]),
+    ],
+  };
 });

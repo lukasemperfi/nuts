b106f2645de64122a0b70cc074ff2742ea43796d
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2025-10-23 17:06:14 +0300

    add products to catalog

--- src/pages/catalog/catalog.js
+++ src/pages/catalog/catalog.js
@@ -1,7 +1,9 @@
 import { initDropdown } from "@/shared/ui/dropdown/dropdown.js";
 import { initWalnutSection } from "@/pages/catalog/sections/walnut/walnut.js";
+import { initProducts } from "@/pages/catalog/sections/products/products.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initDropdown(".top-header__lang");
   initWalnutSection();
+  initProducts();
 });

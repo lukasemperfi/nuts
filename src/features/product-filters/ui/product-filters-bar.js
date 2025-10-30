449c80288bd47f172d65349129482826604506b5
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2025-10-30 14:00:48 +0200

    add loadFilters from url

--- src/features/product-filters/ui/product-filters-bar.js
+++ src/features/product-filters/ui/product-filters-bar.js
@@ -4,12 +4,15 @@ export const initProductFilters = () => {
   let filters = {
     weight: [],
     flavor: [],
-    price: "asc",
+    price: null,
   };
+
+  loadFiltersFromURL();
 
   function updateFilter(type, value) {
     if (type === "price") {
       filters.price = value;
+      updateURL();
       console.log(filters);
       return;
     }
@@ -22,36 +25,74 @@ export const initProductFilters = () => {
       filters[type].push(value);
     }
 
+    updateURL();
+
     console.log(filters);
   }
 
   function resetFilters() {
-    filters = {
-      weight: [],
-      flavor: [],
-      price: "asc",
-    };
+    filters.weight = [];
+    filters.flavor = [];
+    filters.price = null;
+
+    resetURL();
+  }
+
+  function updateURL() {
+    const params = new URLSearchParams();
+
+    filters.weight.forEach((val) => params.append("weight", val));
+
+    filters.flavor.forEach((val) => params.append("flavor", val));
+
+    if (filters.price) {
+      params.set("price", filters.price);
+    }
+
+    const newUrl =
+      window.location.pathname +
+      (params.toString() ? `?${params.toString()}` : "");
+
+    window.history.replaceState({}, "", newUrl);
+  }
+
+  function resetURL() {
+    const newUrl = window.location.pathname;
+    window.history.replaceState({}, "", newUrl);
+
+    console.log("Фильтры сброшены:", filters);
+  }
+
+  function loadFiltersFromURL() {
+    const params = new URLSearchParams(window.location.search);
+
+    filters.weight = params.getAll("weight");
+    filters.flavor = params.getAll("flavor");
+    filters.price = params.get("price") || null;
   }
 
   const flavorSelect = initDropdown({
     selector: ".product-filters-bar__flavor-select",
     onChange: (type, value) => updateFilter(type, value),
-    defaultValues: ["fried", "salted"],
+    defaultValues: [...filters.flavor],
   });
 
   const weightSelect = initDropdown({
     selector: ".product-filters-bar__weight-select",
     onChange: (type, value) => updateFilter(type, value),
+    defaultValues: [...filters.weight],
   });
 
   const desktopPriceButton = initPriceButton({
     selector: ".filter-btn_price-desktop",
     onClick: (type, value) => updateFilter(type, value),
+    defaultValue: filters.price,
   });
 
   const mobilePriceButton = initPriceButton({
     selector: ".filter-btn_price-mobile",
     onClick: (type, value) => updateFilter(type, value),
+    defaultValue: filters.price,
   });
 
   initResetButton({
@@ -67,11 +108,7 @@ export const initProductFilters = () => {
   });
 };
 
-export const initPriceButton = ({
-  selector,
-  defaultValue = "asc",
-  onClick,
-}) => {
+export const initPriceButton = ({ selector, defaultValue, onClick }) => {
   const button = document.querySelector(selector);
   if (!button) {
     return;
@@ -80,22 +117,26 @@ export const initPriceButton = ({
   const iconAsc = button.querySelector(".filter-btn__icon_arrowUp");
   const iconDesc = button.querySelector(".filter-btn__icon_arrowDown");
 
-  let currentValue = defaultValue;
+  let currentValue = defaultValue || null;
 
   const updateIcons = () => {
     if (currentValue === "asc") {
       iconAsc.classList.add("filter-btn__icon_active");
       iconDesc.classList.remove("filter-btn__icon_active");
-    } else {
+    } else if (currentValue === "desc") {
       iconDesc.classList.add("filter-btn__icon_active");
       iconAsc.classList.remove("filter-btn__icon_active");
+    } else {
+      iconAsc.classList.remove("filter-btn__icon_active");
+      iconDesc.classList.remove("filter-btn__icon_active");
     }
   };
 
   updateIcons();
 
   button.addEventListener("click", () => {
-    currentValue = currentValue === "asc" ? "desc" : "asc";
+    currentValue =
+      currentValue === "asc" ? "desc" : currentValue === "desc" ? "asc" : "asc";
     updateIcons();
 
     if (onClick) {
@@ -105,7 +146,7 @@ export const initPriceButton = ({
   });
 
   const reset = () => {
-    currentValue = defaultValue;
+    currentValue = defaultValue || null;
     updateIcons();
   };
 

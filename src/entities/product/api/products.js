e5173bd8f98e6eef77198fb18bf4a8119faf54c3
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2025-10-30 13:07:48 +0200

    add filters methods to api

--- src/entities/product/api/products.js
+++ src/entities/product/api/products.js
@@ -1,23 +1,39 @@
 import { supabase } from "@/shared/api/supabase/client.js";
 
 class Products {
-  getAllProducts = async () => {
-    const { data: products, error } = await supabase
+  getAllProducts = async (filters = {}) => {
+    const {
+      flavorValues = ["raw"],
+      weightValues = [],
+      sortOrder = "desc",
+    } = filters;
+
+    let query = supabase
       .from("products")
       .select(
         `
-      *,
-      product_images (*),
-      product_flavors (
-        flavor_id,
-        flavors (*)
-      ),
-      packaging_types (*),
-      product_statuses (*)
-    `
+        *,
+        product_images (*),
+        product_flavors!inner (
+          flavor_id,
+          flavors!inner (*)
+        ),
+        packaging_types (*),
+        product_statuses (*)
+      `
       )
       .limit(6)
-      .order("created_at", { ascending: false });
+      .order("price", { ascending: sortOrder === "asc" });
+
+    if (flavorValues.length > 0) {
+      query = query.in("product_flavors.flavors.value", flavorValues);
+    }
+
+    if (weightValues.length > 0) {
+      query = query.in("weight", weightValues);
+    }
+
+    const { data: products, error } = await query;
 
     if (error) {
       console.error("Ошибка при получении продуктов:", error.message);
--- src/pages/catalog/sections/products/products.js
+++ src/pages/catalog/sections/products/products.js
@@ -1,6 +1,8 @@
 import { initProductFilters } from "@/features/product-filters/ui/product-filters-bar";
+import { productList } from "@/entities/product/ui/product-list/product-list";
 
 export const initProducts = () => {
   // renderProductList(products, ".products__catalog");
   initProductFilters();
+  // productList(".products__catalog");
 };

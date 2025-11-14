6a99dd4b34722225f95c922ec97e854cfe2341b5
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2025-11-14 22:32:43 +0200

    add tabs content

--- src/pages/payment-delivery/payment-delivery.js
+++ src/pages/payment-delivery/payment-delivery.js
@@ -4,11 +4,13 @@ import { initGoal } from "@/pages/home/sections/goal/goal.js";
 import { lazyLoadElements } from "@/shared/helpers/lazy-loading/lazy-loading.js";
 import { initPageFooter } from "@/widgets/footer/footer.js";
 import { initHeroSection } from "@/pages/payment-delivery/sections/hero/hero.js";
+import { initPaymentDeliveryTabs } from "@/pages/payment-delivery/sections/payment-delivery-tabs/payment-delivery-tabs";
 
 document.addEventListener("DOMContentLoaded", async () => {
   initDropdown({ selector: ".top-header__lang" });
   initHeader();
   initHeroSection();
+  initPaymentDeliveryTabs();
   initGoal();
   initPageFooter();
 

e26f8009986acf9606fb46f995662a79d207412f
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2025-11-27 17:18:56 +0200

    add overlat and redirect login form

--- src/features/auth/login/ui/login-form.js
+++ src/features/auth/login/ui/login-form.js
@@ -3,6 +3,13 @@ import {
   LOGIN_FORM_SELECTORS,
   initLoginFormValidation,
 } from "../model/validation/login";
+import { createOverlaySpinner } from "@/shared/ui/overlay-spinner/overlay-spinner";
+import { store } from "@/app/store";
+import { AUTH_STATUS } from "@/entities/auth/model/auth-slice";
+import { redirect } from "@/shared/helpers/redirect";
+
+const mode = import.meta.env.MODE;
+let baseUrl = mode === "production" ? "/nuts/" : import.meta.env.BASE_URL;
 
 export const initLoginForm = () => {
   initLoginFormValidation().onSuccess(async (event) => {
@@ -12,8 +19,19 @@ export const initLoginForm = () => {
     const formData = new FormData(form);
     const { email, password } = Object.fromEntries(formData.entries());
 
-    await loginUser(email, password);
+    const overlay = createOverlaySpinner("Вы успешно вошли в систему!");
 
-    // console.log("Login validated payload:", payload);
+    store.subscribe("auth", async (newState) => {
+      if (newState.status === AUTH_STATUS.LOADING) {
+        overlay.show();
+      }
+      if (newState.status === AUTH_STATUS.SUCCEEDED) {
+        overlay.success();
+        redirect(baseUrl, 2000);
+        //TODO: clearForm
+      }
+    });
+
+    await loginUser(email, password);
   });
 };

c02c66b92e454917ed16779147bab397a7a78f38
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2025-12-10 16:18:41 +0200

    add active link into menu

--- src/pages/profile/sections/profile-section/profile-section.js
+++ src/pages/profile/sections/profile-section/profile-section.js
@@ -1,1 +1,17 @@
-export const initProfileSection = () => {};
+export const initProfileSection = () => {
+  initAccountMenu();
+};
+
+function initAccountMenu() {
+  const currentUrl = window.location.href;
+  const menuLinks = document.querySelectorAll(".account-menu-list__link");
+
+  menuLinks.forEach((link) => {
+    const linkUrl = link.href;
+
+    if (linkUrl === currentUrl) {
+      link.classList.add("active");
+      link.setAttribute("aria-current", "page");
+    }
+  });
+}

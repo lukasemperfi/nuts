6ea19a846268c45ebf71712d9bd47c05fa7236be
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2025-12-10 17:33:46 +0200

    add toogle menu test variant

--- src/pages/profile/sections/profile-section/profile-section.js
+++ src/pages/profile/sections/profile-section/profile-section.js
@@ -5,6 +5,7 @@ export const initProfileSection = () => {
 function initAccountMenu() {
   const currentUrl = window.location.href;
   const menuLinks = document.querySelectorAll(".account-menu-list__link");
+  toogleAccountMenu();
 
   menuLinks.forEach((link) => {
     const linkUrl = link.href;
@@ -13,5 +14,14 @@ function initAccountMenu() {
       link.classList.add("active");
       link.setAttribute("aria-current", "page");
     }
+  });
+}
+
+function toogleAccountMenu() {
+  const menu = document.querySelector(".profile-section__menu");
+  const toggleBtn = document.querySelector(".user-info-bar__menu-button");
+
+  toggleBtn.addEventListener("click", () => {
+    menu.classList.toggle("is-collapsed");
   });
 }

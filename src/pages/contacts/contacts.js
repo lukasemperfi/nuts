3ec9ef9555f310bb83b03cd479e366fa6815a430
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2025-12-16 00:05:50 +0200

    create contacts page

---
+++ src/pages/contacts/contacts.js
@@ -0,0 +1,12 @@
+import { initDropdown } from "@/shared/ui/dropdown/dropdown.js";
+import { initHeader } from "@/widgets/header/header.js";
+import { lazyLoadElements } from "@/shared/helpers/lazy-loading/lazy-loading.js";
+import { initPageFooter } from "@/widgets/footer/footer.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  initDropdown({ selector: ".top-header__lang" });
+  initHeader();
+  initPageFooter();
+
+  lazyLoadElements(".lazy", { rootMargin: "200px 0px" });
+});
